d5fef93bfb131b727fcc3011f34f58fee1ad3a8a
Pierre de Buyl <pdebuyl@ulb.ac.be>
2013-06-10 17:51:11 +0200

    Modified setup.py for automatic detection of numpy directory.

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import numpy as np
+import os
+
+ran_base = os.path.join(os.path.dirname(np.__file__),'random')
+
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("echange", ["echange_mod.pyx"], extra_objects = ['/usr/lib/pymodules/python2.7/numpy/random/mtrand.so'])],
-    include_dirs=['/usr/lib/pymodules/python2.7/numpy/random/']
+    ext_modules = [Extension("echange", ["echange_mod.pyx"], extra_objects = [os.path.join(ran_base,'mtrand.so')])],
+    include_dirs=[ran_base]
 )
